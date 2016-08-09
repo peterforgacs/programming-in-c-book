ae606d55ca92b3c3cc43358e018feccaedbc4b25
Peter Forgacs <peter.forgacs3@gmail.com>
2016-08-09 04:03:22 +0200

    ex6

---
+++ chapter13/ex6_IS_UPPER_CASE.c
@@ -0,0 +1,12 @@
+/**
+    Write a macro that returns a non zero number if the character is uppercase
+*/
+
+#include <stdio.h>
+#define IS_UPPER_CASE(c)    ( ((c) < 'a') ? 1 : 0  )
+
+int main(void){
+    char c = 'C';
+    printf("%i", IS_UPPER_CASE(c));
+    return 0;
+}
