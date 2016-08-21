66643ae1b8284cf36d05df1060f40517bb543d88
Peter Forgacs <peter.forgacs3@gmail.com>
2016-08-21 23:51:24 +0200

    Renaming arrays

--- chapter16/ex-04-merge_lines.c
+++ chapter16/ex-04-merge_lines.c
@@ -1,2 +1,4 @@
 /**
+    Write a program that merges lines alternately from two fields and writes the result ot stdout.
+    If one file has less lines than the other, the remaining lines from the large file should simply be copied to stdout.
 */
