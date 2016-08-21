426ac78fb6281096b4c33df269115c142439192f
Peter Forgacs <peter.forgacs3@gmail.com>
2016-08-21 23:47:52 +0200

    dispplay_text

---
+++ chapter16/ex-06-display_text.c
@@ -0,0 +1,5 @@
+/**
+    Write a program that display the contents of a file at thee terminal 20 lines at a time.
+    After 20 lines wait for a character to be entered. If the character is q the program should stop.
+    Any other character should invoke the next 20 lines.
+*/
